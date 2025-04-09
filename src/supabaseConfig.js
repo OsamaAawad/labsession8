777916a49eb419ce9ca15d07f0b65fe0bc599b14
Workshop Student <workshopstd@UNIVERSITY.LOCAL>
2025-04-09 14:23:57 +0300

    Storage Update

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,7 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
 
-const URL = "https://xspdkxfmdulurgezlyax.supabase.co"
-const ApiKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzcGRreGZtZHVsdXJnZXpseWF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3ODE4MzAsImV4cCI6MjA1NzM1NzgzMH0.y4yQ43y5Yl25LoOr3GUk77ypVZZs2XNuJJCE4LJY1So"
+const URL = "https://voycdumtaytqpgfcpcmo.supabase.co"
+const ApiKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZveWNkdW10YXl0cXBnZmNwY21vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxOTc1NDYsImV4cCI6MjA1OTc3MzU0Nn0.HAH9T87iF0bQK7vWeZ531vGU4VPrsQV3Jy8Bxco0U2w"
 
 const supbase = createClient(URL,ApiKey)
 
